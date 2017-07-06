09fb00c7ac3fdfd8d80b3a52547c227451ed8047
Thanos_ Bal <athanasios.balanikas@yahoo.gr>
2017-07-07 02:41:52 +0300

    Update Main.java

--- circuit/Main.java
+++ circuit/Main.java
@@ -5,21 +5,19 @@ package circuit;
 public class Main {
     public static void main(String [] args) throws Exception
 	{
-               //Full Adder Circuit
+           
 		 FullAdder f1= new FullAdder();
                
-                //A builds Circuit for 2 integers: a, b
+           
                 f1.getCircuit();
                 
-                //B receives encrypted circuit and provides intermediate results: carrier bit and sum bit
-                //3 inputs: a, b, c, c: carrier bit
+                
                 boolean FA_ab_result[]= f1.evaluate(true, false, true); 
                
-                //A builds Circuit for 2 integers: Sj, d 
+                
                 f1.getCircuit();
                 
-                //B receives encrypted circuit and provides intermediate results: carrier bit and sum bit
-                //3 inputs: Sj, d, c, c: carrier bit
+                
                 boolean FA_abc_result[]= f1.evaluate(FA_ab_result[1], false, true); //Sj + d
   
                 System.out.println("");
@@ -28,21 +26,19 @@ public class Main {
                 
                 System.out.println("");
                 
-                //Integer Equality - Socialist Millionaires Problem
+                
                 IntEquality f= new IntEquality();
                 
-                //A builds Circuit for 2 integers: a, b
+                
                 f.getCircuit();
                 
-                //B receives encrypted circuit and provides intermediate results: carrier bit
-                //3 inputs: a, b, c, c: carrier bit
+                
                 boolean ab_result= f.evaluate(true, true, true); // a comp b
                 
-                //A builds Circuit for 2 integers: Cj, d 
+                
                 f.getCircuit();
                 
-                //B receives encrypted circuit and provides intermediate results: carrier bit
-                //3 inputs: Cj, d, c, c: carrier bit
+               
                 boolean abc_result= f.evaluate(ab_result, false, true); //a comp b comp d
                 
                 
